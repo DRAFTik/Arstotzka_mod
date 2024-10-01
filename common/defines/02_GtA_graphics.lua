e02792903c9941a666aa196b97cc8af5a25dd920
DRAFTik <41925073+DRAFTik@users.noreply.github.com>
2024-10-01 19:36:51 +0300

    Ультра мап фикс

---
+++ common/defines/02_GtA_graphics.lua
@@ -0,0 +1,22 @@
+-- BASE
+NDefines_Graphics.NFrontend.CAMERA_LOOKAT_X = 1525.0			-- 2958.0			-- Rotation point in main menu
+NDefines_Graphics.NFrontend.CAMERA_LOOKAT_Z = 700.0				-- 1519.0
+
+NDefines_Graphics.NFrontend.CAMERA_START_X = 1525.0				-- 2958.0			-- Initial position in main menu
+NDefines_Graphics.NFrontend.CAMERA_START_Y = 0.0				-- 800.0
+NDefines_Graphics.NFrontend.CAMERA_START_Z = 700.0				-- 1400.0
+
+NDefines_Graphics.NFrontend.CAMERA_END_X = 1525.0				-- 2958.0			-- Move to position in main menu
+NDefines_Graphics.NFrontend.CAMERA_END_Z = 700.0				-- 1400.0
+
+NDefines_Graphics.NFrontend.FRONTEND_POS_X = 1525.0				-- 2958.0
+NDefines_Graphics.NFrontend.FRONTEND_POS_Z = 700.0				-- 1500.0
+
+NDefines_Graphics.NFrontend.FRONTEND_LOOK_X = 1525.0			-- 2958.0
+NDefines_Graphics.NFrontend.FRONTEND_LOOK_Z = 700.0				-- 1519.0
+
+
+NDefines_Graphics.NFrontend.MP_OPTIONS_POS_X = 1525.0			-- 2958.0
+NDefines_Graphics.NFrontend.MP_OPTIONS_POS_Z = 700.0			-- 848.0
+NDefines_Graphics.NFrontend.MP_OPTIONS_LOOK_X = 1525.0			-- 2958.0
+NDefines_Graphics.NFrontend.MP_OPTIONS_LOOK_Z = 700.0			-- 1519.0
